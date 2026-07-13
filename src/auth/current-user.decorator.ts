d10680afe746216aaf43693b14b8b3e7e6455fb8
Librariann <okpc0306@naver.com>
2026-07-13 12:00:00 +0900

    Add src/auth/current-user.decorator.ts

---
+++ src/auth/current-user.decorator.ts
@@ -0,0 +1,6 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { User } from "../entities/user.entity";
+export const CurrentUser = createParamDecorator(
+  (_data: unknown, ctx: ExecutionContext): User =>
+    ctx.switchToHttp().getRequest<{ user: User }>().user,
+);
