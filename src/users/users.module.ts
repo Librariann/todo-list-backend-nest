788992f612bd4904e0d5936a4d364fd9a29edcc5
Librariann <okpc0306@naver.com>
2026-08-06 12:00:00 +0900

    Update src/users/users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserPoint } from "../entities/user-point.entity";
+import { UserReward } from "../entities/reward.entity";
 import { User } from "../entities/user.entity";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, UserPoint, UserReward])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
